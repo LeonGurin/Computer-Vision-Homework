31dec82416af71085774c573d438625d86b8d6de
Itai Bear <itai.bear1@gmail.com>
2023-04-22 15:57:48 +0300

    The first 4 affine puzzles are now complete 🥳

--- HW1/script.py
+++ HW1/script.py
@@ -46,6 +46,12 @@ def read_sorted_pieces(pieces_dir):
     
     return pieces
 
+def plot_best_matches(piece1, piece2, best_matches, kp1, kp2):
+    dm_best_matches = [cv.DMatch(_queryIdx=m[0], _trainIdx=m[1], _imgIdx=0, _distance=0) for m in best_matches]
+    result = cv.drawMatches(piece1, kp1, piece2, kp2, dm_best_matches, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+    plt.imshow(result), plt.show()
+
 def solve_puzzle(directory):
     pieces_dir = os.path.join(directory, 'pieces')
     # find the txt file in the directory using a one-liner given there is only one
@@ -61,50 +67,60 @@ def solve_puzzle(directory):
     # read pieces
     pieces = read_sorted_pieces(pieces_dir)
     gray_pieces = [cv.cvtColor(piece, cv.COLOR_BGR2GRAY) for piece in pieces]
+
+    gray_target = cv.warpPerspective(gray_pieces[0], global_transform, (target_width, target_height), flags=cv.INTER_CUBIC)
+    target = cv.warpPerspective(pieces[0], global_transform, (target_width, target_height), flags=cv.INTER_CUBIC)
+    target_mask = cv.warpPerspective(np.ones_like(gray_pieces[0]), global_transform, (target_width, target_height), flags=cv.INTER_CUBIC)
     
-    # find keypoints and descriptors for all pieces
-    sift = cv.SIFT_create()
-    kp, des = zip(*[sift.detectAndCompute(piece, None) for piece in gray_pieces])
+    for i in range(1, len(pieces)):
 
-    # plot keypoints for a single test piece
-    test_piece = cv.drawKeypoints(pieces[1], kp[1], None)
-    cv.imshow("First Piece Keypoints", test_piece)
-    print("Number of keypoints in first piece: ", len(kp[0]))
+        # find keypoints and descriptors for all pieces
+        sift = cv.SIFT_create()
+        #kp, des = zip(*[sift.detectAndCompute(piece, None) for piece in gray_pieces])
+        target_kp, target_des = sift.detectAndCompute(gray_target, None)
+        kp, des = sift.detectAndCompute(gray_pieces[i], None)
 
-    distances = calculate_distances_matrix(des[1], des[0])
-    best_matches = find_best_matches(distances, threshold=0.75)
-    print("Number of best matches: ", len(best_matches))
+        # plot keypoints for a single test piece
+        test_piece = cv.drawKeypoints(pieces[i], kp, None)
+        cv.imshow("First Piece Keypoints", test_piece)
+        print("Number of keypoints in first piece: ", len(kp))
 
-    # implement RANSAC to find the best transformation matrix
-    best_error = np.inf
-    all_src_pts = np.float32([kp[1][match[0]].pt for match in best_matches]).reshape(-1, 1, 2)
-    all_dst_pts = np.float32([kp[0][match[1]].pt for match in best_matches]).reshape(-1, 1, 2)
-    for i in range(10000):
-        # select 3 random points
-        random_matches = random.sample(best_matches, k=3)
-        # calculate transformation matrix
-        src_pts = np.float32([kp[1][match[0]].pt for match in random_matches]).reshape(-1, 1, 2)
-        dst_pts = np.float32([kp[0][match[1]].pt for match in random_matches]).reshape(-1, 1, 2)
-        M = cv.getAffineTransform(src_pts, dst_pts)
-        # calculate the residuals for all the points
-        transformed_points = cv.transform(all_src_pts, M)
-        residuals = [euclidean_distance(p, all_dst_pts) for p in transformed_points]
-        error = np.sum(residuals)
-        # if the number of inliers is larger than the current best, update the best
-        if error < best_error:
-            best_error = error
-            transform = M
-        
-    # apply the transformation matrix to the test piece
-    print("Transformation matrix: ", transform)
-    transform = np.vstack((transform, np.array([0, 0, 1])))
-    transformed_piece = cv.warpPerspective(pieces[1], np.dot(global_transform, transform), (target_width, target_height), flags=cv.INTER_CUBIC)
-    cv.imshow("Transformed Piece", transformed_piece)
+        distances = calculate_distances_matrix(des, target_des)
+        best_matches = find_best_matches(distances, threshold=0.75)
+        print("Number of best matches: ", len(best_matches))
+        #plot_best_matches(pieces[1], target, best_matches, kp, target_kp)
 
-    target = np.zeros((target_height, target_width, 3), dtype=np.uint8)
-    first_piece_transformed = cv.warpPerspective(pieces[0], global_transform, (target_width, target_height), flags=cv.INTER_CUBIC)
-    target = cv.add(target, first_piece_transformed)
-    target = cv.add(target, transformed_piece)
+        # implement RANSAC to find the best transformation matrix
+        best_error = np.inf
+        all_src_pts = np.float32([kp[match[0]].pt for match in best_matches]).reshape(-1, 1, 2)
+        all_dst_pts = np.float32([target_kp[match[1]].pt for match in best_matches]).reshape(-1, 1, 2)
+        for _ in range(1000):
+            # select 3 random points
+            random_matches = random.sample(best_matches, k=3)
+            # calculate transformation matrix
+            src_pts = np.float32([kp[match[0]].pt for match in random_matches]).reshape(-1, 1, 2)
+            dst_pts = np.float32([target_kp[match[1]].pt for match in random_matches]).reshape(-1, 1, 2)
+            M = cv.getAffineTransform(src_pts, dst_pts)
+            # calculate the residuals for all the points
+            transformed_points = cv.transform(all_src_pts, M)
+            residuals = [euclidean_distance(tp, dp) for tp, dp in zip(transformed_points, all_dst_pts)]
+            error = np.mean(residuals)
+            # if the number of inliers is larger than the current best, update the best
+            if error < best_error:
+                best_error = error
+                transform = M
+            
+
+
+        # apply the transformation matrix to the test piece
+        transform = np.vstack((transform, np.array([0, 0, 1])))
+        print("Transformation matrix: ", transform)
+        transformed_piece = cv.warpPerspective(pieces[i], transform, (target_width, target_height), flags=cv.INTER_CUBIC)
+        gray_transformed_piece = cv.warpPerspective(gray_pieces[i], transform, (target_width, target_height), flags=cv.INTER_CUBIC)
+        #cv.imshow("Transformed Piece", transformed_piece)
+        target[gray_target == 0] = transformed_piece[gray_target == 0]
+        gray_target[gray_target == 0] = gray_transformed_piece[gray_target == 0]
+        #target = cv.add(target, transformed_piece)
 
     cv.imshow("Final Image", target)
 
@@ -114,7 +130,7 @@ def solve_puzzle(directory):
 
 
 def main():
-    solve_puzzle('puzzles/puzzle_affine_1')
+    solve_puzzle('puzzles/puzzle_affine_5')
     cv.waitKey(0)
     print('done')
 
