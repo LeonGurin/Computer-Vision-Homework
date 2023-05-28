fd8166c7b022eb0cedf9106ce6eab927e059549d
Leon Gurin <leon9gur@gmail.com>
2023-05-28 03:46:46 +0300

    we got results but still more work needed 🔮
    needs fixing but works. it. works.

--- HW2/sol.py
+++ HW2/sol.py
@@ -7,116 +7,169 @@ with open('set_1/max_disp.txt', 'r') as f:
     MAX_DISP = int(f.read())
     f.close()
 
-WINDOW_SIZE = 3 # window size for census transform
+with open('set_1/K.txt', 'r') as f:
+    K = np.array([[float(num) for num in line.split()] for line in f])
+    f.close()
 
+WINDOW_SIZE_X = 11
+WINDOW_SIZE_Y = 5
+BASELINE = 1
+FOCAL_LENGTH = K[0, 0]
 
 def census_transform(img):
     h, w = img.shape
     census = np.zeros((h, w), dtype=np.uint64)
 
-    start = WINDOW_SIZE // 2
+    start_x = WINDOW_SIZE_X // 2
+    start_y = WINDOW_SIZE_Y // 2
 
-    for i in range(start, h - start):
-        for j in range(start, w - start):
+    for i in range(start_x, h - start_x):
+        for j in range(start_y, w - start_y):
             binary = ''
-            # iterate over window
-            for k in range(-start, start + 1):
-                for l in range(-start, start + 1):
-                    if img[i, j] >= img[i + k, j + l]:
+            for k in range(i - start_x, i + start_x + 1):
+                for l in range(j - start_y, j + start_y + 1):
+                    if img[k, l] >= img[i, j]:
                         binary += '1'
                     else:
                         binary += '0'
             census[i, j] = int(binary, 2)
     return census
 
-# def hamming_distance(left, right):
+def hamming_distance(left, right):
     h, w = left.shape
-    hamming = np.zeros((h, w), dtype=np.uint8)
-    for i in range(1, h-1):
-        for j in range(1, w-1):
+    hamming = np.zeros((h, w), dtype=np.uint64)
+    for i in range(h):
+        for j in range(w):
             hamming[i, j] = bin(left[i, j] ^ right[i, j]).count('1')
     return hamming
 
-def cost_volume(left_census, right_census, d=MAX_DISP):
+def cost_volume(left_census, right_census):
     h, w = left_census.shape
-    cost_volume = np.zeros((h, w, d), dtype=np.uint64)
+    cost_volume1 = np.zeros((h, w, MAX_DISP), dtype=np.uint64)
+    cost_volume2 = np.zeros((h, w, MAX_DISP), dtype=np.uint64)
 
-    for i in range(d):
-        shifted_right_census = np.roll(right_census, i, axis=1)
-        diff = np.count_nonzero(left_census != shifted_right_census)
-        cost_volume[:, :, i] = diff
+    right_shifted = right_census.copy()
+    left_shifted = left_census.copy()
 
-    return cost_volume
+    for i in range(MAX_DISP):
+            right_shifted = np.roll(right_census, i, axis=1)
+            hamming = hamming_distance(left_census, right_shifted)
+            cost_volume1[:, :, i] = hamming
 
-def aggregate_cost_volume(cost_volume):
-    h, w, d = cost_volume.shape
-    aggregated_cost_volume = np.zeros((h, w, d), dtype=np.uint64)
-    
-    mask_size = (3, 3)
+            left_shifted = np.roll(left_census, -i, axis=1)
+            hamming = hamming_distance(right_census, left_shifted)
+            cost_volume2[:, :, i] = hamming
+
+    return cost_volume1, cost_volume2
+
+def aggregate_cost_volume(cost_volume1, cost_volume2):
+    mask_size = (7, 7)
     # perform uniform averaging with openCV filter
-    for i in range(d):
-        aggregated_cost_volume[:, :, i] = cv.blur(cost_volume[:, :, i], mask_size)
+    for i in range(MAX_DISP):
+        cost_volume1[:, :, i] = cv.blur(cost_volume1[:, :, i], mask_size)
+        cost_volume2[:, :, i] = cv.blur(cost_volume2[:, :, i], mask_size)
     
-    return aggregated_cost_volume
+    return cost_volume1, cost_volume2
 
 def find_minimum_cost(aggregated_cost_volume):
-    h, w, d = aggregated_cost_volume.shape
+    h, w, _ = aggregated_cost_volume.shape
     disparity = np.zeros((h, w), dtype=np.uint64)
-    for i in range(1, h-1):
-        for j in range(1, w-1):
-            # find minimum cost
-            disparity[i, j] = np.argmin(aggregated_cost_volume[i, j, :])
+    
+    disparity = np.argmin(aggregated_cost_volume, axis=2)
+
+    # for i in range(h):
+    #     for j in range(w):
+    #         min_val = aggregated_cost_volume[i, j, 0]
+    #         for d in range(MAX_DISP):
+    #             if aggregated_cost_volume[i, j, d] < min_val:
+    #                 min_val = aggregated_cost_volume[i, j, d]
+
+    #         # disparity[i, j] = np.argmin(aggregated_cost_volume[i, j, :])
+    #         disparity[i, j] = min_val
     
     return disparity
 
-def consistency_test(disparity, aggregated_cost_volume):
-    h, w, _ = aggregated_cost_volume.shape
+def consistency_test(left_disparity, right_disparity):
+    h, w = left_disparity.shape
+    threshold = 30
+    
+    for i in range(h):
+        for j in range(w):
+            mapped_val = j - left_disparity[i, j]
+            if mapped_val >= 0 and mapped_val < w:
+                if abs(left_disparity[i, j] - right_disparity[i, mapped_val]) > threshold:
+                    left_disparity[i, j] = 0
+                    right_disparity[i, mapped_val] = 0
+            
+            mapped_val = j - right_disparity[i, j]
+            if mapped_val >= 0 and mapped_val < w:
+                if abs(right_disparity[i, j] - left_disparity[i, mapped_val]) > threshold:
+                    right_disparity[i, j] = 0
+                    left_disparity[i, mapped_val] = 0
+    
+    return left_disparity, right_disparity
 
-    for y in range(h):
-        for x in range(w):
-            # Find the corresponding pixel position in the right image
-            corresponding_x = x - disparity[y, x]
+def create_depth_map(disparity):
+    # depth_map = np.where(disparity != 0, (BASELINE * FOCAL_LENGTH) / disparity, 0.0)
+    # return depth_map
 
-            # Retrieve the disparity value from the right image
-            if 0 <= corresponding_x < w:
-                corresponding_disparity = disparity[y, corresponding_x]
+    h, w = disparity.shape
+    depth_map = np.zeros((h, w), dtype=np.float32)
+    for i in range(h):
+        for j in range(w):
+            if disparity[i, j] != 0:
+                depth_map[i, j] = (BASELINE * FOCAL_LENGTH) / disparity[i, j]
             else:
-                corresponding_disparity = 0
-
-            # Compare the retrieved disparity value with the initial disparity value
-            diff = abs(disparity[y, x] - corresponding_disparity)
-
-            # Perform the consistency test
-            if diff <= 5:
-                disparity[y, x] = 255  # Mark as consistent
-    return disparity                
+                depth_map[i, j] = 0.0
+    
+    return depth_map
 
 def main():
-    left = cv.imread('set_1/im_left.jpg', cv.IMREAD_GRAYSCALE)
-    right = cv.imread('set_1/im_right.jpg', cv.IMREAD_GRAYSCALE)
-    
+    left  = cv.imread('example/im_left.jpg', cv.IMREAD_GRAYSCALE)
+    right = cv.imread('example/im_right.jpg', cv.IMREAD_GRAYSCALE)
+
     # calculate census transform
     left_census = census_transform(left)
     right_census = census_transform(right)
     
-    # calculate cost volume
-    cost_vol = cost_volume(left_census, right_census)
-    
+    # calculate cost volumes
+    leftToRight_cost_vol, rightToLeft_cost_vol = cost_volume(left_census, right_census)
+
     # aggregate cost volume
-    aggregated_cost_volume = aggregate_cost_volume(cost_vol)
-    
+    leftToRight_aggregated_cost_vol, rightToLeft_aggregated_cost_vol = aggregate_cost_volume(leftToRight_cost_vol, rightToLeft_cost_vol)
+
     # find minimum cost
-    disparity = np.argmin(aggregated_cost_volume, axis=2)
-    # disparity = find_minimum_cost(aggregated_cost_volume)
-    
+    # leftToRight_disparity = np.argmin(leftToRight_aggregated_cost_vol, axis=2)
+    # rightToLeft_disparity = np.argmin(rightToLeft_aggregated_cost_vol, axis=2)
+    leftToRight_disparity = find_minimum_cost(leftToRight_aggregated_cost_vol)
+    rightToLeft_disparity = find_minimum_cost(rightToLeft_aggregated_cost_vol)
+
     # filter with consistency test
-    # disparity = consistency_test(disparity, aggregated_cost_volume)
+    leftToRight_disparity, rightToLeft_disparity = consistency_test(leftToRight_disparity, rightToLeft_disparity)
     
-    cv.imwrite('disparity_map.png', disparity)
-    # plt.imshow(disparity, cmap='gray')
-    # plt.imshow(left_census, cmap='gray')
-    # plt.imshow(right_census, cmap='gray')
-    plt.show()
+    # create depth map
+    depth_left = create_depth_map(leftToRight_disparity).astype(np.uint8)
+    depth_right = create_depth_map(rightToLeft_disparity).astype(np.uint8)
+    # depth_left = np.where(leftToRight_disparity != 0, (BASELINE * FOCAL_LENGTH) / leftToRight_disparity, 0)
+    # depth_right = np.where(rightToLeft_disparity != 0, (BASELINE * FOCAL_LENGTH) / rightToLeft_disparity, 0)
+
+    # depth_left = cv.applyColorMap(depth_left, cv.COLORMAP_JET)
+    # depth_right = cv.applyColorMap(depth_right, cv.COLORMAP_JET)
+
+    cv.imwrite('leftToRight_disparity.png', leftToRight_disparity)
+    cv.imwrite('rightToLeft_disparity.png', rightToLeft_disparity)
+    cv.imwrite('depth_left.png', depth_left)
+    cv.imwrite('depth_right.png', depth_right)
+
+    # display results
+    # cv.imshow('left', left)
+    # cv.imshow('right', right)
+    # cv.imshow('leftToRight_disparity', leftToRight_disparity)
+    # cv.imshow('rightToLeft_disparity', rightToLeft_disparity)
+    cv.imshow('depth_left', depth_left)
+    cv.imshow('depth_right', depth_right)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
